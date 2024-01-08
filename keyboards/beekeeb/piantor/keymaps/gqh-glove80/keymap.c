2de5a685862aad5aeaeae8984eba597b78c184ad
Gert Qin Hansen <gertqin@gmail.com>
2024-01-08 01:33:16 +0100

    colemak dk, single sym

--- keyboards/beekeeb/piantor/keymaps/gqh-glove80/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/gqh-glove80/keymap.c
@@ -9,8 +9,14 @@
 
 
 enum LAYERS {
-    _DEF,
+    _COLEMAK,
+    _COLEMAK_DK,
     _SYM,
+};
+
+enum custom_keycodes {
+    KC_COLEMAK = SAFE_RANGE,
+    KC_COLE_DK,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -20,17 +26,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //     KC_GRV,          LCTL_T(KC_Z),  LALT_T(KC_X),  LGUI_T(KC_C),  KC_V,    KC_B,                KC_N,  KC_M,  RGUI_T(KC_COMM), LALT_T(KC_DOT),  RCTL_T(KC_SLSH),  OSM(MOD_LALT),
     //                                            OSM(MOD_LSFT),  KC_BSPC,  LT(_SYM, KC_TAB),      LT(_SYM, KC_ENT),  KC_SPC,  OSM(MOD_RSFT)
     // ),
-    [_DEF] = LAYOUT_split_3x6_3(
-        KC_EQL,          KC_Q,          KC_W,          KC_F,          KC_P,  KC_G,                KC_J,  KC_L,  KC_U,            KC_Y,            KC_SCLN,          KC_MINS,
-        LGUI_T(KC_ESC),  KC_A,          KC_R,          KC_S,          KC_T,  KC_D,                KC_H,  KC_N,  KC_E,            KC_I,            KC_O,             RGUI_T(KC_QUOT),
-        KC_GRV,          LCTL_T(KC_Z),  LALT_T(KC_X),  LGUI_T(KC_C),  KC_V,  KC_B,                KC_K,  KC_M,  RGUI_T(KC_COMM), LALT_T(KC_DOT),  RCTL_T(KC_SLSH),  OSM(MOD_LALT),
-                                               OSM(MOD_LSFT),  KC_BSPC,  LT(_SYM, KC_TAB),      LT(_SYM, KC_ENT),  KC_SPC,  OSM(MOD_RSFT)
+    [_COLEMAK] = LAYOUT_split_3x6_3(
+        KC_EQL,          KC_Q,          KC_W,          KC_F,  KC_P,  KC_B,                KC_J,  KC_L,  KC_U,    KC_Y,            KC_SCLN,          KC_MINS,
+        KC_ESC,          KC_A,          KC_R,          KC_S,  KC_T,  KC_G,                KC_H,  KC_N,  KC_E,    KC_I,            KC_O,             KC_QUOT,
+        LGUI_T(KC_GRV),  LCTL_T(KC_Z),  LALT_T(KC_X),  KC_C,  KC_D,  KC_V,                KC_K,  KC_M,  KC_COMM, LALT_T(KC_DOT),  RCTL_T(KC_SLSH),  RGUI_T(KC_BSLS),
+                                          OSM(MOD_LSFT),  KC_BSPC,  LT(_SYM, KC_TAB),      LT(_SYM, KC_ENT),  KC_SPC,  OSM(MOD_RSFT)
+    ),
+    [_COLEMAK_DK] = LAYOUT_split_3x6_3(
+        KC_EQL,          KC_Q,          KC_W,          KC_F,  KC_P,  KC_B,                KC_J,  KC_L,  KC_U,    KC_Y,            A(KC_O),          A(KC_A),
+        KC_ESC,          KC_A,          KC_R,          KC_S,  KC_T,  KC_G,                KC_H,  KC_N,  KC_E,    KC_I,            KC_O,             A(KC_QUOT),
+        LGUI_T(KC_GRV),  LCTL_T(KC_Z),  LALT_T(KC_X),  KC_C,  KC_D,  KC_V,                KC_K,  KC_M,  KC_COMM, LALT_T(KC_DOT),  RCTL_T(KC_SLSH),  RGUI_T(KC_QUOT),
+                                          OSM(MOD_LSFT),  KC_BSPC,  LT(_SYM, KC_TAB),      LT(_SYM, KC_ENT),  KC_SPC,  OSM(MOD_RSFT)
     ),
     [_SYM] = LAYOUT_split_3x6_3(
-        KC_TRNS,  S(KC_1),       S(KC_2),       S(KC_3),       S(KC_4),  S(KC_5),                   S(KC_6),     S(KC_7),  S(KC_8),       S(KC_9),       S(KC_0),       KC_TRNS,
-        KC_TRNS,  LCTL_T(KC_1),  LALT_T(KC_2),  LGUI_T(KC_3),  KC_4,     KC_5,                      KC_6,        KC_7,     RGUI_T(KC_8),  LALT_T(KC_9),  RCTL_T(KC_0),  KC_TRNS,
-        KC_TRNS,  KC_LBRC,       KC_RBRC,       KC_LEFT,       KC_RGHT,  KC_BSLS,                   S(KC_BSLS),  KC_DOWN,  KC_UP,         S(KC_LBRC),    S(KC_RBRC),    KC_TRNS,
-                                               OSM(MOD_LSFT),  KC_BSPC,  LT(_SYM, KC_TAB),      LT(_SYM, KC_ENT),  KC_SPC,  OSM(MOD_RSFT)
+        KC_TRNS,  S(KC_1),       S(KC_2),       S(KC_3),  S(KC_4),   S(KC_5),              S(KC_6),     S(KC_7),     S(KC_8),     S(KC_9),  S(KC_0),         KC_MINS,
+        KC_TRNS,  KC_0,          KC_1,          KC_2,     KC_3,      KC_4,                 KC_LEFT,     KC_DOWN,     KC_UP,       KC_RGHT,  DF(KC_COLEMAK),  KC_QUOT,
+        KC_TRNS,  LCTL_T(KC_5),  LALT_T(KC_6),  KC_7,     KC_8,      KC_9,                 KC_LBRC,     S(KC_LBRC),  S(KC_RBRC),  KC_RBRC,  DF(KC_COLE_DK),  RGUI_T(KC_BSLS),
+                                               OSM(MOD_LSFT),  A(KC_BSPC),  LT(_SYM, KC_TAB),      LT(_SYM, KC_ENT),  KC_SPC,  OSM(MOD_RSFT)
     )
 };
 
@@ -38,6 +50,8 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LT(_SYM, KC_TAB):
         case LT(_SYM, KC_ENT):
+        case LGUI_T(KC_GRV):
+        case RGUI_T(KC_BSLS):
             // Immediately select the hold action when another key is pressed.
             return true;
         default:
