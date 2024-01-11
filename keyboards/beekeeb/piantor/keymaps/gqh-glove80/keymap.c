3d70542bc385d7fdd42229b088c19a94d6f224c4
Gert Qin Hansen <gertqin@gmail.com>
2024-01-11 02:45:28 +0100

    Shift middle thumb

--- keyboards/beekeeb/piantor/keymaps/gqh-glove80/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/gqh-glove80/keymap.c
@@ -31,25 +31,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,                 KC_Q,          KC_W,          KC_F,          KC_P,  KC_B,                KC_J,  KC_L,  KC_U,            KC_Y,            KC_SCLN,          KC_MINS,
         KC_ESC,                 KC_A,          KC_R,          KC_S,          KC_T,  KC_G,                KC_H,  KC_N,  KC_E,            KC_I,            KC_O,             KC_QUOT,
         LT(_SYM_LEFT, KC_GRV),  LCTL_T(KC_Z),  LALT_T(KC_X),  LGUI_T(KC_C),  KC_D,  KC_V,                KC_K,  KC_M,  RGUI_T(KC_COMM), LALT_T(KC_DOT),  RCTL_T(KC_SLSH),  KC_BSLS,
-                                             OSM(MOD_LSFT),  KC_BSPC,  LT(_SYM_RIGHT, KC_TAB),      LT(_SYM_LEFT, KC_ENT),  KC_SPC,  OSM(MOD_RSFT)
+                                             KC_BSPC,  OSM(MOD_LSFT),  LT(_SYM_RIGHT, KC_TAB),      LT(_SYM_LEFT, KC_ENT),  KC_SPC,  KC_DEL
     ),
     [_COLEMAK_DK] = LAYOUT_split_3x6_3(
         KC_EQL,                 KC_Q,          KC_W,          KC_F,          KC_P,  KC_B,                KC_J,  KC_L,  KC_U,            KC_Y,            A(KC_O),          A(KC_A),
         KC_ESC,                 KC_A,          KC_R,          KC_S,          KC_T,  KC_G,                KC_H,  KC_N,  KC_E,            KC_I,            KC_O,             A(KC_QUOT),
         LT(_SYM_LEFT, KC_GRV),  LCTL_T(KC_Z),  LALT_T(KC_X),  LGUI_T(KC_C),  KC_D,  KC_V,                KC_K,  KC_M,  RGUI_T(KC_COMM), LALT_T(KC_DOT),  RCTL_T(KC_SLSH),  KC_QUOT,
-                                            OSM(MOD_LSFT),  KC_BSPC,  LT(_SYM_RIGHT, KC_TAB),      LT(_SYM_LEFT, KC_ENT),  KC_SPC,  OSM(MOD_RSFT)
+                                             KC_BSPC,  OSM(MOD_LSFT),  LT(_SYM_RIGHT, KC_TAB),      LT(_SYM_LEFT, KC_ENT),  KC_SPC,  KC_DEL
     ),
     [_SYM_LEFT] = LAYOUT_split_3x6_3(
         S(KC_EQL),  S(KC_1),       S(KC_2),       S(KC_3),  S(KC_4),   S(KC_5),              KC_F6,     KC_F7,    KC_F8,    KC_F9,           KC_F10,          KC_MINS,
-        KC_TRNS,    KC_1,          KC_2,          KC_3,     KC_4,      KC_5,                 S(KC_6),   KC_MINS,  S(KC_8),  XXXXXXX,         KC_RGUI,         KC_QUOT,
+        KC_TRNS,    KC_1,          KC_2,          KC_3,     KC_4,      KC_5,                 KC_SCLN,   KC_RSFT,  KC_LGUI,  KC_LALT,         KC_RCTL,         KC_QUOT,
         S(KC_GRV),  LCTL_T(KC_6),  LALT_T(KC_7),  KC_8,     KC_9,      KC_0,                 XXXXXXX,   KC_F11,   KC_F12,   DF(KC_COLEMAK),  DF(KC_COLE_DK),  KC_BSLS,
-                                           OSM(MOD_LSFT),  A(KC_BSPC),  KC_TAB,      KC_ENT,  KC_SPC,  OSM(MOD_RSFT)
+                                           A(KC_BSPC),  OSM(MOD_LSFT),  KC_TAB,      KC_ENT,  KC_SPC,  KC_DEL
     ),
     [_SYM_RIGHT] = LAYOUT_split_3x6_3(
-        KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                          S(KC_7),     S(KC_LBRC),  S(KC_9),     S(KC_0),    S(KC_SCLN),  S(KC_MINS),
-        KC_TRNS,  KC_LCTL,  KC_LALT,  KC_LGUI,  KC_LSFT,  XXXXXXX,                        KC_LEFT,     KC_DOWN,     KC_UP,       KC_RGHT,    S(KC_RBRC),  S(KC_QUOT),
-        KC_TRNS,  G(KC_Z),  G(KC_X),  G(KC_C),  G(KC_V),  SGUI(KC_Z),                     KC_LBRC,     KC_RBRC,     S(KC_COMM),  S(KC_DOT),  S(KC_SLSH),  S(KC_BSLS),
-                                           OSM(MOD_LSFT),  A(KC_BSPC),  KC_TAB,      KC_ENT,  KC_SPC,  OSM(MOD_RSFT)
+        KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                          S(KC_6),     S(KC_7),     S(KC_8),     S(KC_9),  S(KC_0),  KC_MINS,
+        KC_TRNS,  KC_LCTL,  KC_LALT,  KC_LGUI,  KC_LSFT,  XXXXXXX,                        KC_LEFT,     KC_DOWN,     KC_UP,       KC_RGHT,  XXXXXXX,  KC_QUOT,
+        KC_TRNS,  G(KC_Z),  G(KC_X),  G(KC_C),  G(KC_V),  SGUI(KC_Z),                     KC_LBRC,     S(KC_LBRC),  S(KC_RBRC),  KC_RBRC,  XXXXXXX,  KC_BSLS,
+                                           A(KC_BSPC),  OSM(MOD_LSFT),  KC_TAB,      KC_ENT,  KC_SPC,  KC_DEL
     )
 };
 
