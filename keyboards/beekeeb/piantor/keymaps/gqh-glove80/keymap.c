cc4203258f6c017f1302a89b7bf48ae9518f29a1
Gert Qin Hansen <gertqin@gmail.com>
2024-12-19 10:31:36 +0100

    First version of consolidating sym and nav layer

--- keyboards/beekeeb/piantor/keymaps/gqh-glove80/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/gqh-glove80/keymap.c
@@ -23,30 +23,44 @@ enum LAYERS {
 #define LT_NAV_ENT LT(_NAV, KC_ENT)
 #define LCTL_ENT LCTL_T(KC_ENT)
 
+#define LT_NAV_ESC LT(_NAV, KC_ESC)
+#define ALT_ENT LALT_T(KC_ENT)
+#define CTL_GRV LCTL_T(KC_GRV)
+#define CTL_COLN RCTL_T(KC_COLN)
+#define GUI_TAB LGUI_T(KC_TAB)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MAIN] = LAYOUT_split_3x6_3(
         KC_SCLN,     KC_V,   KC_L,   KC_N,   KC_D,     KC_K,                                   KC_J,   KC_W,   KC_O,     KC_U,      KC_QUOT,   KC_MINS,
-        LT_SYM_ESC,  KC_T,   KC_S,   KC_R,   KC_H,     KC_F,                                   KC_G,   KC_C,   KC_A,     KC_I,      KC_E,      A(KC_BSPC),
-        OSM_LSFT,    KC_Z,   KC_Q,   KC_X,   KC_P,     KC_B,                                   KC_M,   KC_Y,   KC_DOT,   KC_COMM,   KC_DQUO,   OSM_RSFT,
-                                              OSM_LGUI,  KC_SPC,  LCTL_ENT,         MO(_NAV),  QK_REP,  OSM_LALT
+        LT_NAV_ESC,  KC_T,   KC_S,   KC_R,   KC_H,     KC_F,                                   KC_G,   KC_C,   KC_A,     KC_I,      KC_E,      A(KC_BSPC),
+        CTL_GRV,     KC_Z,   KC_Q,   KC_X,   KC_P,     KC_B,                                   KC_M,   KC_Y,   KC_DOT,   KC_COMM,   KC_DQUO,   CTL_COLN,
+                                              GUI_TAB,  KC_SPC,  ALT_ENT,            OSM_LSFT,  QK_REP,  QK_AREP
     ),
+    // [_NAV] = LAYOUT_split_3x6_3(
+    //     KC_F12,    KC_1,       KC_2,      KC_3,      KC_4,      KC_5,                          KC_6,     KC_7,      KC_8,     KC_9,      KC_0,      KC_TRNS,
+    //     KC_TRNS,   OSM_LCTL,   OSM_LALT,  OSM_LGUI,  OSM_LSFT,  XXXXXXX,                       XXXXXXX,  KC_LEFT,   KC_DOWN,  KC_UP,     KC_RIGHT,  KC_BSPC,
+    //     KC_TRNS,   S(KC_TAB),  KC_LBRC,   KC_RBRC,   KC_TAB,    KC_GRV,                        KC_GRV,   KC_TAB,    KC_DOT,   KC_COMM,   KC_SLSH,   KC_TRNS,
+    //                                           KC_TRNS,  KC_TRNS, KC_ENT,              KC_TRNS,  KC_TRNS,  KC_TRNS
+    // ),
+    // [_SYM] = LAYOUT_split_3x6_3(
+    //     KC_TRNS,   XXXXXXX,    S(KC_2),  S(KC_3),  S(KC_4),    S(KC_5),                        KC_PIPE,   KC_SCLN,  KC_LBRC,  KC_RBRC,   KC_LCBR,  KC_RCBR,
+    //     KC_TRNS,   XXXXXXX,    KC_ASTR,  KC_SLSH,  KC_MINS,    KC_PLUS,                        KC_AMPR,   KC_EQL,   KC_LPRN,  KC_RPRN,   KC_EXLM,  KC_BSPC,
+    //     KC_TRNS,   XXXXXXX,    KC_2,     KC_1,     KC_0,       KC_TILD,                        KC_GRV,    KC_COLN,  KC_LT,    KC_GT,     KC_DQUO,  KC_BSLS,
+    //                                           KC_TRNS,  KC_TRNS, KC_ENT,              KC_TRNS,  KC_TRNS,   KC_TRNS
+    // )
     [_NAV] = LAYOUT_split_3x6_3(
-        KC_F12,    KC_1,       KC_2,      KC_3,      KC_4,      KC_5,                          KC_6,     KC_7,      KC_8,     KC_9,      KC_0,      KC_TRNS,
-        KC_TRNS,   OSM_LCTL,   OSM_LALT,  OSM_LGUI,  OSM_LSFT,  XXXXXXX,                       XXXXXXX,  KC_LEFT,   KC_DOWN,  KC_UP,     KC_RIGHT,  KC_BSPC,
-        KC_TRNS,   S(KC_TAB),  KC_LBRC,   KC_RBRC,   KC_TAB,    KC_GRV,                        KC_GRV,   KC_TAB,    KC_DOT,   KC_COMM,   KC_SLSH,   KC_TRNS,
-                                              KC_TRNS,  KC_TRNS, KC_ENT,              KC_TRNS,  KC_TRNS,  KC_TRNS
+        KC_F12,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,                          KC_6,     KC_7,      KC_8,     KC_9,   KC_0,      KC_TRNS,
+        KC_TRNS,   KC_LCTL,   KC_LBRC,   KC_LPRN,   KC_RPRN,   KC_RBRC,                       KC_AMPR,  KC_LEFT,   KC_DOWN,  KC_UP,  KC_RIGHT,  KC_BSPC,
+        KC_TRNS,   XXXXXXX,   KC_BSLS,   KC_EXLM,   KC_EQL,    KC_MINS,                       KC_PIPE,  KC_ASTR,   KC_LT,    KC_GT,  KC_SLSH,   KC_TRNS,
+                                              KC_TRNS,  KC_TRNS, KC_TRNS,              KC_TRNS,  KC_TRNS,  KC_TRNS
     ),
-    [_SYM] = LAYOUT_split_3x6_3(
-        KC_TRNS,   XXXXXXX,    S(KC_2),  S(KC_3),  S(KC_4),    S(KC_5),                        KC_PIPE,   KC_SCLN,  KC_LBRC,  KC_RBRC,   KC_LCBR,  KC_RCBR,
-        KC_TRNS,   XXXXXXX,    KC_ASTR,  KC_SLSH,  KC_MINS,    KC_PLUS,                        KC_AMPR,   KC_EQL,   KC_LPRN,  KC_RPRN,   KC_EXLM,  KC_BSPC,
-        KC_TRNS,   XXXXXXX,    KC_2,     KC_1,     KC_0,       KC_TILD,                        KC_GRV,    KC_COLN,  KC_LT,    KC_GT,     KC_DQUO,  KC_BSLS,
-                                              KC_TRNS,  KC_TRNS, KC_ENT,              KC_TRNS,  KC_TRNS,   KC_TRNS
-    )
 };
 
-const uint16_t PROGMEM ent_combo[] = {KC_G, KC_C, COMBO_END};
+const uint16_t PROGMEM cmd_spc_combo[] = {KC_SPC, QK_AREP, COMBO_END};
+const uint16_t PROGMEM cmd_ent_combo[] = {ALT_ENT, QK_AREP, COMBO_END};
 combo_t key_combos[] = {
-    COMBO(ent_combo, KC_ENT),
+    COMBO(cmd_spc_combo, G(KC_SPC)),
+    COMBO(cmd_ent_combo, G(KC_ENT)),
 };
 
 const key_override_t spc_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_SPC, KC_UNDS);
@@ -54,7 +68,6 @@ const key_override_t alt_bspc_key_override = ko_make_basic(MOD_MASK_SHIFT, A(KC_
 const key_override_t bspc_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 const key_override_t dot_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_EXLM);
 const key_override_t comma_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, S(KC_SLSH));
-const key_override_t quote_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_QUOT, KC_GRV);
 const key_override_t x_cmd_key_override = ko_make_basic(MOD_MASK_GUI, KC_X, G(KC_C));
 const key_override_t scln_cmd_key_override = ko_make_basic(MOD_MASK_GUI, KC_SCLN, G(KC_TAB));
 const key_override_t i_alt_key_override = ko_make_basic(MOD_MASK_ALT, KC_I, A(KC_QUOT));
@@ -67,19 +80,30 @@ const key_override_t *key_overrides[] = {
 	&bspc_key_override,
 	&dot_key_override,
 	&comma_key_override,
-	&quote_key_override,
 	&x_cmd_key_override,
 	&scln_cmd_key_override,
 	&i_alt_key_override
 };
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case CTL_COLN:
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(KC_COLN); // Send KC_DQUO on tap
+                return false;        // Return false to ignore further processing of key
+            }
+            break;
+    }
+    return true;
+}
+
 uint16_t get_alt_repeat_key_keycode_user(uint16_t keycode, uint8_t mods) {
      if (!mods) {
         switch (keycode) {
-            case KC_SPC:
-                return OSM_LSFT;
+            case KC_E: return KC_B;
+            case KC_B: return KC_E;
         }
     }
 
-    return QK_REP;
+    return KC_TRNS;
 }
