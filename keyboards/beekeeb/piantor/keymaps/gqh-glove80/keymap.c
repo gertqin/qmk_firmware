1eed3c4ffce6aa2ca12ed355aa1cbc2206782cf2
Gert Qin Hansen <gertqin@gmail.com>
2024-12-22 03:19:25 +0100

    Use OSL + LLOCK for sym layer

--- keyboards/beekeeb/piantor/keymaps/gqh-glove80/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/gqh-glove80/keymap.c
@@ -7,6 +7,8 @@
 
 #include QMK_KEYBOARD_H
 
+#define LAYER_LOCK_IDLE_TIMEOUT 60000  // Turn off after 60 seconds.
+
 #define OSM_LCTL OSM(MOD_LCTL)
 #define OSM_LALT OSM(MOD_LALT)
 #define OSM_LGUI OSM(MOD_LGUI)
@@ -15,34 +17,33 @@
 
 enum LAYERS {
     _MAIN,
-    _NAV,
+    // _NAV,
     _SYM,
 };
   
-#define LT_SYM_ESC LT(_SYM, KC_ESC)
-#define LT_NAV_ENT LT(_NAV, KC_ENT)
-#define LCTL_ENT LCTL_T(KC_ENT)
+// #define LT_SYM_ESC LT(_SYM, KC_ESC)
+// #define LT_NAV_ENT LT(_NAV, KC_ENT)
+// #define LCTL_ENT LCTL_T(KC_ENT)
 
-#define LT_NAV_ESC LT(_NAV, KC_ESC)
 #define ALT_ENT LALT_T(KC_ENT)
-#define CTL_GRV LCTL_T(KC_GRV)
+#define CTL_ESC LCTL_T(KC_ESC)
 #define CTL_COLN RCTL_T(KC_COLN)
 #define GUI_TAB LGUI_T(KC_TAB)
-#define CTL_4 LCTL_T(KC_4)
-#define ALT_5 LALT_T(KC_5)
-#define GUI_6 LGUI_T(KC_6)
+#define CTL_LCK LCTL_T(KC_0)
+#define ALT_4 LALT_T(KC_4)
+#define GUI_5 LGUI_T(KC_5)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MAIN] = LAYOUT_split_3x6_3(
         KC_SCLN,     KC_V,   KC_L,   KC_N,   KC_D,     KC_K,                                   KC_J,   KC_W,   KC_O,     KC_U,      KC_QUOT,   KC_MINS,
-        LT_NAV_ESC,  KC_T,   KC_S,   KC_R,   KC_H,     KC_F,                                   KC_G,   KC_C,   KC_A,     KC_I,      KC_E,      A(KC_BSPC),
-        CTL_GRV,     KC_Z,   KC_Q,   KC_X,   KC_P,     KC_B,                                   KC_M,   KC_Y,   KC_DOT,   KC_COMM,   KC_DQUO,   CTL_COLN,
+        OSL(_SYM),   KC_T,   KC_S,   KC_R,   KC_H,     KC_F,                                   KC_G,   KC_C,   KC_A,     KC_I,      KC_E,      A(KC_BSPC),
+        CTL_ESC,     KC_Z,   KC_Q,   KC_X,   KC_P,     KC_B,                                   KC_M,   KC_Y,   KC_DOT,   KC_COMM,   KC_DQUO,   CTL_COLN,
                                               GUI_TAB,  KC_SPC,  ALT_ENT,            OSM_LSFT,  QK_REP,  QK_AREP
     ),
-    [_NAV] = LAYOUT_split_3x6_3(
-        KC_F12,    KC_EXLM,   KC_1,   KC_2,   KC_3,   KC_PLUS,                                 KC_PIPE,  KC_LBRC,   KC_LPRN,  KC_RPRN,  KC_RBRC,   KC_LCBR,
-        KC_TRNS,   KC_EXLM,   CTL_4,  ALT_5,  GUI_6,  KC_0,                                    KC_AMPR,  KC_LEFT,   KC_DOWN,  KC_UP,    KC_RIGHT,  KC_BSPC,
-        KC_TRNS,   XXXXXXX,   KC_7,   KC_8,   KC_9,   KC_MINS,                                 KC_ASTR,  KC_EQL,    KC_LT,    KC_GT,    KC_SLSH,   KC_RCBR,
+    [_SYM] = LAYOUT_split_3x6_3(
+        KC_F12,    KC_EXLM,   KC_1,   KC_2,   KC_3,   KC_PLUS,                                 KC_PIPE,  KC_LBRC,   KC_LPRN,  KC_RPRN,  KC_RBRC,   KC_SCLN,
+        KC_TRNS,   CTL_LCK,   ALT_4,  GUI_5,  KC_6,   KC_0,                                    KC_AMPR,  KC_LEFT,   KC_DOWN,  KC_UP,    KC_RIGHT,  KC_BSPC,
+        QK_LLCK,   KC_BSLS,   KC_7,   KC_8,   KC_9,   KC_MINS,                                 KC_ASTR,  KC_EQL,    KC_LT,    KC_GT,    KC_SLSH,   KC_TILD,
                                               KC_TRNS,  KC_TRNS, KC_TRNS,              KC_TRNS,  KC_TRNS,  KC_TRNS
     ),
     // [_NAV] = LAYOUT_split_3x6_3(
@@ -70,6 +71,7 @@ const key_override_t alt_bspc_key_override = ko_make_basic(MOD_MASK_SHIFT, A(KC_
 const key_override_t bspc_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 const key_override_t dot_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_EXLM);
 const key_override_t comma_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, S(KC_SLSH));
+const key_override_t quot_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_QUOT, KC_GRV);
 const key_override_t x_cmd_key_override = ko_make_basic(MOD_MASK_GUI, KC_X, G(KC_C));
 const key_override_t scln_cmd_key_override = ko_make_basic(MOD_MASK_GUI, KC_SCLN, G(KC_TAB));
 const key_override_t i_alt_key_override = ko_make_basic(MOD_MASK_ALT, KC_I, A(KC_QUOT));
@@ -82,9 +84,11 @@ const key_override_t *key_overrides[] = {
 	&bspc_key_override,
 	&dot_key_override,
 	&comma_key_override,
+	&quot_key_override,
 	&x_cmd_key_override,
 	&scln_cmd_key_override,
-	&i_alt_key_override
+	&i_alt_key_override,
+	&alt_slash_key_override
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -92,6 +96,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case CTL_COLN:
             if (record->tap.count && record->event.pressed) {
                 tap_code16(KC_COLN);
+                return false;
+            }
+            break;
+        case CTL_LCK:
+            if (record->tap.count) {
+                if (record->event.pressed) {
+                    // Toggle the lock on the highest layer.
+                    layer_lock_invert(get_highest_layer(layer_state));
+                }
                 return false;
             }
             break;
